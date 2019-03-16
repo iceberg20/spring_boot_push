62e5d990cd9a6dcfe89366c2695d94169535141d
iceberg20 <italoberg@yahoo.com.br>
2019-03-15 21:42:30 -0300

    initial coomit

---
+++ src/main/java/com/push/proto_push/ProtoPushApplication.java
@@ -0,0 +1,14 @@
+package com.push.proto_push;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoPushApplication.class, args);
+                System.out.println(" ######## meu log ##########");
+	}
+
+}
---
+++ src/test/java/com/push/proto_push/ProtoPushApplicationTests.java
@@ -0,0 +1,16 @@
+package com.push.proto_push;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProtoPushApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
